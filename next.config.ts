2bac4fd630f05cd71e2b24444c1018ed03d23e53
truerilind <truerilind@gmail.com>
2026-01-31 02:52:22 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
